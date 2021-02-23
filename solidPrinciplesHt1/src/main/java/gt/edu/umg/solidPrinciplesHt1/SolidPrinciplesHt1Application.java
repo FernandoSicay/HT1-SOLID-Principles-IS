ff7914744b5e6c7684cf596acec6f12dca1ce02e
Fernando Sicay <fsicayc@miumg.edu.gt>
2021-02-23 13:07:45 -0600

    iniciando projecto principios SOLID

---
+++ solidPrinciplesHt1/src/main/java/gt/edu/umg/solidPrinciplesHt1/SolidPrinciplesHt1Application.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.solidPrinciplesHt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidPrinciplesHt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidPrinciplesHt1Application.class, args);
+	}
+
+}
